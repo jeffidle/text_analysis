c1160aaa833a4c863e9af592bce0dc5ce8998f73
Jeff Idle <jidle1975@gmail.com>
2020-02-04 01:03:07 -0600

    Script updates

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 # Define server logic to read selected file ----
-server <- function(input, output) {
+server <- function(input, output, session) {
         
-        output$data_contents <- renderTable({
+        text_data <- reactive({
                 
                 # input$file1 will be NULL initially. After the user selects
                 # and uploads a file, head of that data file by default,
@@ -9,21 +9,38 @@ server <- function(input, output) {
                 
                 req(input$file1)
                 
-                data_df <- read.csv(input$file1$datapath,
-                               header = input$header,
-                               sep = input$sep,
-                               quote = input$quote)
+                if(input$filetype1 == "CSV"){
+                        
+                        data_df <- read.csv(input$file1$datapath,
+                                               header = input$header1,
+                                               stringsAsFactors = FALSE,
+                                               sep = ",",
+                                               quote = input$quote1)
+                        
+                } else {
+                        
+                        data_df <- read_xlsx(input$file1$datapath,
+                                                col_names = input$header1,
+                                                trim_ws = TRUE)
+                        
+                }
                 
-                if(input$disp == "head") {
+                if(input$disp1 == "head") {
                         return(head(data_df))
                 }
                 else {
                         return(data_df)
                 }
+        })
+        
+        output$data_contents <- renderTable({
+                
+                options(shiny.maxRequestSize = 1000*1024^2)
+                text_data()    
                 
         })
         
-        output$library_contents <- renderTable({
+        library_data <- reactive({
                 
                 # input$file1 will be NULL initially. After the user selects
                 # and uploads a file, head of that data file by default,
@@ -31,18 +48,45 @@ server <- function(input, output) {
                 
                 req(input$file2)
                 
-                library_df <- read.csv(input$file2$datapath,
-                                    header = input$header,
-                                    sep = input$sep,
-                                    quote = input$quote)
+                if(input$filetype2 == "CSV"){
+                        
+                        library_df <- read.csv(input$file2$datapath,
+                                               header = input$header2,
+                                               stringsAsFactors = FALSE,
+                                               sep = ",",
+                                               quote = input$quote2)
+                                
+                } else {
+                        
+                        library_df <- read_xlsx(input$file2$datapath,
+                                               col_names = input$header2,
+                                               trim_ws = TRUE)
+                        
+                }
                 
-                if(input$disp == "head") {
+                
+                if(input$disp2 == "head") {
                         return(head(library_df))
                 }
                 else {
                         return(library_df)
                 }
                 
+        })
+        
+        output$library_contents <- renderTable({
+                
+                options(shiny.maxRequestSize = 30*1024^2)
+                
+                library_data()
+                
+        })
+        
+        observe({
+                updateSelectInput(session, "library_name",
+                                  label = "library_name",
+                                  choices = library_data()$names,
+                                  selected = library_data()$names[1])
         })
         
 }
--- ui.R
+++ ui.R
@@ -5,33 +5,33 @@ ui <- fluidPage(
         titlePanel("Analyze your text"),
         
         tabsetPanel(type = "tabs",
-                    tabPanel("(1) Load your data", sidebarLayout(
+                    tabPanel("(1) Load your data >>", sidebarLayout(
                             
                             # Sidebar panel for inputs ----
                             sidebarPanel(
                                     
                                     # Input: Select a file ----
-                                    fileInput("file1", "Choose CSV File",
+                                    fileInput("file1", "Choose CSV or XLSX data file",
                                               multiple = TRUE,
                                               accept = c("text/csv",
                                                          "text/comma-separated-values,text/plain",
-                                                         ".csv")),
+                                                         ".csv",
+                                                         ".xlsx")),
                                     
                                     # Horizontal line ----
                                     tags$hr(),
                                     
                                     # Input: Checkbox if file has header ----
-                                    checkboxInput("header", "Header", TRUE),
+                                    checkboxInput("header1", "Header", TRUE),
                                     
                                     # Input: Select separator ----
-                                    radioButtons("sep", "Separator",
-                                                 choices = c(Comma = ",",
-                                                             Semicolon = ";",
-                                                             Tab = "\t"),
-                                                 selected = ","),
+                                    radioButtons("filetype1", "File Type",
+                                                 choices = c("CSV" = "CSV",
+                                                             "XLSX" = "XLSX"),
+                                                 selected = "CSV"),
                                     
                                     # Input: Select quotes ----
-                                    radioButtons("quote", "Quote",
+                                    radioButtons("quote1", "Quote",
                                                  choices = c(None = "",
                                                              "Double Quote" = '"',
                                                              "Single Quote" = "'"),
@@ -41,7 +41,7 @@ ui <- fluidPage(
                                     tags$hr(),
                                     
                                     # Input: Select number of rows to display ----
-                                    radioButtons("disp", "Display",
+                                    radioButtons("disp1", "Display",
                                                  choices = c(Head = "head",
                                                              All = "all"),
                                                  selected = "head")
@@ -58,33 +58,33 @@ ui <- fluidPage(
                             
                     )),
                     
-                    tabPanel("(2) Load your library", sidebarLayout(
+                    tabPanel("(2) Load your library >>", sidebarLayout(
                             
                             # Sidebar panel for inputs ----
                             sidebarPanel(
                                     
                                     # Input: Select a file ----
-                                    fileInput("file2", "Choose CSV File",
+                                    fileInput("file2", "Choose CSV or XLSX library file",
                                               multiple = TRUE,
                                               accept = c("text/csv",
                                                          "text/comma-separated-values,text/plain",
-                                                         ".csv")),
+                                                         ".csv",
+                                                         ".xlsx")),
                                     
                                     # Horizontal line ----
                                     tags$hr(),
                                     
                                     # Input: Checkbox if file has header ----
-                                    checkboxInput("header", "Header", TRUE),
+                                    checkboxInput("header2", "Header", TRUE),
                                     
                                     # Input: Select separator ----
-                                    radioButtons("sep", "Separator",
-                                                 choices = c(Comma = ",",
-                                                             Semicolon = ";",
-                                                             Tab = "\t"),
-                                                 selected = ","),
+                                    radioButtons("filetype2", "File Type",
+                                                 choices = c("CSV" = "CSV",
+                                                             "XLSX" = "XLSX"),
+                                                 selected = "CSV"),
                                     
                                     # Input: Select quotes ----
-                                    radioButtons("quote", "Quote",
+                                    radioButtons("quote2", "Quote",
                                                  choices = c(None = "",
                                                              "Double Quote" = '"',
                                                              "Single Quote" = "'"),
@@ -94,7 +94,7 @@ ui <- fluidPage(
                                     tags$hr(),
                                     
                                     # Input: Select number of rows to display ----
-                                    radioButtons("disp", "Display",
+                                    radioButtons("disp2", "Display",
                                                  choices = c(Head = "head",
                                                              All = "all"),
                                                  selected = "head")
@@ -111,15 +111,18 @@ ui <- fluidPage(
                             
                     )),
                     
-                    tabPanel("(3) Choose how you want to map and clean your data",
+                    tabPanel("(3) Choose how you want to map and clean your data >>",
                              
-                             column(6, selectInput("library_name", "Select the column that contains the names of your libraries:", choices = ifelse(exists("library_df", sort(names(library_df)), "Please load your library")))),
-                             column(6, selectInput("library_terms", "Select the column that contains your library terms:", choices = ifelse(exists("library_df", sort(names(library_df)), "Please load your library")))),
-                             column(6, selectInput("unique_id", "Select the unique record identifier in your data:", choices = ifelse(exists("data_df", sort(names(data_df)), "Please load your data")))),
-                             column(6, selectInput("text_data", "Select the text you want to analyze:", choices = ifelse(exists("data_df", sort(names(data_df)), "Please load your data")))),
-                             column(12, selectInput("data_cleaning", "Choose the data cleansing functions you want to apply to your data:", 
-                                                    c("Convert to lowercase", "Replace abbreviations", "Replace numbers", "Replace symbols", "Remove contractions"),
-                                                    selected = NULL, multiple = TRUE)))
+                             column(6, selectInput("library_name", "Select the column that contains the names of your libraries:", choices = "")),
+                             column(6, selectInput("library_terms", "Select the column that contains your library terms:", choices = ifelse(exists("library_df"), sort(names(library_df)), "Please upload your library"))),
+                             column(6, selectInput("unique_id", "Select the unique record identifier in your data:", choices = ifelse(exists("data_df"), sort(names(data_df)), "Please upload your data"))),
+                             column(6, selectInput("text_data", "Select the text you want to analyze:", choices = ifelse(exists("data_df"), sort(names(data_df)), "Please upload your data"))),
+                             column(12, checkboxGroupInput("data_cleaning", "Choose the data cleansing functions you want to apply to your data:", 
+                                                    c("Convert to lowercase", "Replace abbreviations", "Replace numbers", "Replace symbols", "Remove contractions")))),
+                    
+                    tabPanel("(4) View output >>", "For each comment:  character count, word count, polarity score, negative words, positive words, library word flag, library word count, library words found, risk scale (0-1)"),
+                    
+                    tabPanel("(5) Output summary", "Output summary by dictionary")
                     
         )
         
